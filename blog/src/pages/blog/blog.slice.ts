285c6e64ea485c63567fb4a3acfaf1d89d829043
nguyenngochuy00 <ngochuy25120@gmail.com>
2024-07-09 11:20:41 +0700

    Create action deletePost with createAsyncThunk

--- blog/src/pages/blog/blog.slice.ts
+++ blog/src/pages/blog/blog.slice.ts
@@ -17,14 +17,14 @@ const initialState: BlogState = {
 // Dùng createAsyncThunk ở extraReducers, dùng ở reducers sẽ genarate ra action (ko cần thiết)
 // _ : khi ko khai báo gì
 export const getPostList = createAsyncThunk('blog/getPostList', async (_, thunkAPI) => {
-  const response = await http.get<Post[]>('post', {
+  const response = await http.get<Post[]>('posts', {
     signal: thunkAPI.signal
   })
   return response.data
 })
 
 export const addPost = createAsyncThunk('blog/addPost', async (body: Omit<Post, 'id'>, thunkAPI) => {
-  const response = await http.post<Post>('post', body, {
+  const response = await http.post<Post>('posts', body, {
     signal: thunkAPI.signal
   })
   return response.data
@@ -33,12 +33,19 @@ export const addPost = createAsyncThunk('blog/addPost', async (body: Omit<Post, 
 export const updatePost = createAsyncThunk(
   'blog/updatePost',
   async ({ postId, body }: { postId: string; body: Post }, thunkAPI) => {
-    const response = await http.put<Post>(`post/${postId}`, body, {
+    const response = await http.put<Post>(`posts/${postId}`, body, {
       signal: thunkAPI.signal
     })
     return response.data
   }
 )
+
+export const deletePost = createAsyncThunk('blog/deletePost', async (postId: string, thunkAPI) => {
+  const response = await http.delete<Post>(`posts/${postId}`, {
+    signal: thunkAPI.signal
+  })
+  return response.data
+})
 
 // export const addPost = createAction('blog/addPost', function (post: Omit<Post, 'id'>) {
 //   return {
@@ -58,13 +65,13 @@ const blogSlice = createSlice({
   name: 'blog',
   initialState,
   reducers: {
-    deletePost: (state, action: PayloadAction<string>) => {
-      const postId = action.payload
-      const foundPostIndex = state.postList.findIndex((post) => post.id === postId)
-      if (foundPostIndex !== -1) {
-        state.postList.splice(foundPostIndex, 1)
-      }
-    },
+    // deletePost: (state, action: PayloadAction<string>) => {
+    //   const postId = action.payload
+    //   const foundPostIndex = state.postList.findIndex((post) => post.id === postId)
+    //   if (foundPostIndex !== -1) {
+    //     state.postList.splice(foundPostIndex, 1)
+    //   }
+    // },
     startEditingPost: (state, action: PayloadAction<string>) => {
       const postId = action.payload
       const foundPost = state.postList.find((post) => post.id === postId) || null
@@ -72,18 +79,18 @@ const blogSlice = createSlice({
     },
     cancelEditingPost: (state) => {
       state.editingPost = null
-    },
-    finishEditingPost: (state, action: PayloadAction<Post>) => {
-      const postId = action.payload.id
-      state.postList.some((post, index) => {
-        if ((post.id = postId)) {
-          state.postList[index] = action.payload
-          return true
-        }
-        return false
-      })
-      state.editingPost = null
     }
+    // finishEditingPost: (state, action: PayloadAction<Post>) => {
+    //   const postId = action.payload.id
+    //   state.postList.some((post, index) => {
+    //     if ((post.id = postId)) {
+    //       state.postList[index] = action.payload
+    //       return true
+    //     }
+    //     return false
+    //   })
+    //   state.editingPost = null
+    // }
     // addPost: {
     //   reducer: (state, action: PayloadAction<Post>) => {
     //     const post = action.payload
@@ -121,6 +128,13 @@ const blogSlice = createSlice({
           return false
         })
         state.editingPost = null
+      })
+      .addCase(deletePost.fulfilled, (state, action) => {
+        const postId = action.meta.arg
+        const deletePostIndex = state.postList.findIndex((post) => post.id === postId)
+        if (deletePostIndex !== -1) {
+          state.postList.splice(deletePostIndex, 1)
+        }
       })
       .addMatcher(
         (action) => action.type.includes('cancel'),
@@ -171,7 +185,7 @@ const blogSlice = createSlice({
 //     })
 // })
 
-export const { cancelEditingPost, deletePost, finishEditingPost, startEditingPost } = blogSlice.actions
+export const { cancelEditingPost, startEditingPost } = blogSlice.actions
 const blogReducer = blogSlice.reducer
 
 export default blogReducer
--- blog/src/utils/http.ts
+++ blog/src/utils/http.ts
@@ -4,7 +4,7 @@ class Http {
   instance: AxiosInstance
   constructor() {
     this.instance = axios.create({
-      baseURL: 'http://localhost:4000',
+      baseURL: 'http://localhost:4000/',
       timeout: 10000
     })
   }
